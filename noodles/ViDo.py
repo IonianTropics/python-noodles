284f0243bcee2212af4f739bc21ba61ec89ac485
Darrowynn <63286932+Darrowynn@users.noreply.github.com>
2020-12-23 14:06:42 -0800

    Add files via upload

---
+++ noodles/ViDo.py
@@ -0,0 +1,15 @@
+"""
+PYTHON APPLICATION
+Date: 9/5/2020 12:07 PM
+Format: UTF-8
+License: MIT
+"""
+# example url: https://www.youtube.com/watch?v=mpkf_p71rKY
+
+# u = input("Enter URL:\n")
+# if u == "example":
+#     u = "https://www.youtube.com/watch?v=mpkf_p71rKY"
+#
+# vid = YouTube(url=u)
+#
+# print(vid.streams.first().download())
